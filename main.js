a262a50aa0b175bd7322eb6af8f9e76b73f7dfa7
Cindy <cindy.ol@hotmail.fr>
2021-06-08 09:55:46 +0200

    main

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ Menu.setApplicationMenu(menu)
 
 app.whenReady()
     .then(() => {
-        this.createWindow(800, 600, './src/views/index.html', false, 'index')
+        createWindow(800, 600, './src/views/index.html', false, 'index')
     })
 
 app.on('windows-all-closed', () => {
@@ -16,16 +16,24 @@ app.on('windows-all-closed', () => {
 
 let windowsArray = []
 
+const createOrShowWindow = (width, height, file, top, windowTitle) => {
+    if (getWindow(windowTitle)) {
+        getWindow(windowTitle).show();
+    } else {
+        createWindow(width, height, './src/views/' + file, top, windowTitle);
+    }
+}
+
 const getWindow = (windowTitle) => {
     for (var i = 0; i < windowsArray.length; i++) {
         if (windowsArray[i].webContents.browserWindowOptions.title == windowTitle) {
-        return windowsArray[i];
+            return windowsArray[i];
         }
     }
-  return null;
+    return null;
 }
 
-exports.createWindow = (width, height, file, top, title) => {
+const createWindow = (width, height, file, top, title) => {
     let win = new BrowserWindow({
         width: width,
         height: height,
@@ -47,21 +55,59 @@ exports.createWindow = (width, height, file, top, title) => {
 }
 
 ipcMain.on('getRole', (event, data) => {
+    createOrShowWindow(400, 600, "newWindow.html", false, 'newWindow');
     getWindow("newWindow").webContents.send('dataFromMain', data)
     getWindow("index").hide()
 })
 
 ipcMain.on('getRapportId', (event, data) => {
-    console.log(data)
+    createOrShowWindow(400, 600, 'ficheRapport.html', false, 'ficheRapport')
     getWindow("ficheRapport").webContents.send('rapportIdFromMain', data)
 })
 
 ipcMain.on('updateRapportById', (event, data) => {
-    console.log(data)
+    createOrShowWindow(400, 600, 'updateRapport.html', false, 'modifierRapport')
     getWindow("modifierRapport").webContents.send('updateRapportIdFromMain', data)
 })
 
 ipcMain.on('createRapport', (event, data) => {
-    console.log(data)
+    createOrShowWindow(400, 600, 'creerRapport.html', false, 'creerRapport')
     getWindow("creerRapport").webContents.send('createRapportFromMain', data)
+})
+
+ipcMain.on("deconnexion", () => {
+    getWindow("index").show();
+    for (var i = 0; i < windowsArray.length; i++) {
+        if (windowsArray[i].webContents.browserWindowOptions.title !== "index") {
+            windowsArray[i].hide();
+        }
+    }
+})
+
+ipcMain.on("deleteRapport", (event, data) => {
+    if (getWindow("ficheRapport")) {
+        getWindow("ficheRapport").hide();
+    }
+    getWindow("newWindow").webContents.send("deleteRapportFromMain", data)
+})
+
+ipcMain.on("createRapportClose", (event, data) => {
+    getWindow("newWindow").webContents.send("dataFromMain", data);
+    getWindow("creerRapport").hide()
+})
+
+ipcMain.on("updateWindowClose", () => {
+    getWindow("modifierRapport").hide()
+})
+
+ipcMain.on("createWindowClose", () => {
+    getWindow("creerRapport").hide()
+})
+
+ipcMain.on("rapportModifie", (event, data) => {
+    if (getWindow("ficheRapport")) {
+        getWindow("ficheRapport").hide()
+    }
+    getWindow("newWindow").webContents.send("dataFromMain", data)
+    getWindow("modifierRapport").hide()
 })
